55a0f7ac69a9bc9732816cae9c1336063c39618f
kumax0 <Canerunus@gmail.com>
2022-01-27 11:36:43 +0100

    created movies, planets, characters, header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './_components/movies/movies.component';
+import { CharactersComponent } from './_components/characters/characters.component';
+import { PlanetsComponent } from './_components/planets/planets.component';
+import { HeaderComponent } from './_components/header/header.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    CharactersComponent,
+    PlanetsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
